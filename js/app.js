e6dd1838aac7f347e7b422809ed34578d09e35d0
Alex <alexka002@gmail.com>
2019-01-03 23:59:52 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,86 @@
+let form = document.querySelector("#bookForm");
+
+form.addEventListener('submit', function(e)
+{
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+    const book = new Book(title,author,isbn);
+    e.preventDefault();
+    const ui = new UI();
+    
+  
+    if (title === '' || author ==='' || isbn === '')
+    {
+        ui.showAlert('Please fill everything in!', 'error');
+    } else {
+        ui.addBook(book);
+        ui.clearFields();
+        ui.showAlert('Successfully Added!', 'success');
+    }
+});
+
+// ui.prototype.addBook = function(book)
+// {
+//     console.log(book);
+// }
+
+
+class Book
+{
+    constructor(title, author, isbn)
+    {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// function addBook(e)
+//     {
+        
+//         const title = document.querySelector('#title').value;
+//         const author = document.querySelector('#author').value;
+//         const isbn = document.querySelector('#isbn').value;
+//         const book = new Book(title,author,isbn);
+//         console.log(book);
+//         e.preventDefault();
+//     }
+
+class UI
+{
+    addBook(book)
+    {   
+        const list = document.querySelector('#libraryList');
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X<a></td>`
+        list.appendChild(row);
+        
+        
+    }
+    
+    showAlert(message, className)
+    {
+        const div = document.createElement('div');
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        container.insertBefore(div, form);
+        setTimeout(function(){div.remove()}, 3000);
+    }
+    deleteBook(target)
+    {
+
+    }
+    clearFields()
+    {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
+
+}
